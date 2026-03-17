527041c65761c872028a8c2f4d13bcd7c054ba89
divine isaac <divineisaac00@gmail.com>
2026-03-17 13:23:59 +0100

    Add utility classes and animations for improved UI interactions; update HTML structure for better navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,30 @@ module.exports = {
   content: ["./index.html", "./src/**/*.html"],
   theme: {
     extend: {
-      screens:{
+      screens: {
         tab: "800px",
       },
+      keyframes: {
+        bounces: {
+          "0%": {
+            transform: "scaleY(0)",
+          },
+          "80%": {
+            transform: "scaleY(1.3)",
+          },
+          "100%": {
+            transform: "scaleY(1)",
+          },
+        },
+      },
+      animation:{
+        'bounces': 'bounces 0.5s ease-in-out forwards',
+      },
       fontFamily: {
-        'mona': ['Mona Sans', 'sans-serif'],
-        'bebas': ['"Bebas Neue"', 'sans-serif'],
-        'dm-serif': ['"DM Serif Display"', 'serif'],
-      }
+        mona: ["Mona Sans", "sans-serif"],
+        bebas: ['"Bebas Neue"', "sans-serif"],
+        "dm-serif": ['"DM Serif Display"', "serif"],
+      },
     },
   },
   plugins: [],
